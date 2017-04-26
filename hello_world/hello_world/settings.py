27dc8307b51e0601f3dc06416892206422560292
Chathan Driehuys <cdriehuys@gmail.com>
2017-04-25 20:08:44 -0400

    Add local settings import.

--- hello_world/hello_world/settings.py
+++ hello_world/hello_world/settings.py
@@ -118,3 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Attempt to import local settings
+try:
+    from hello_world.local_settings import *
+except ImportError:
+    pass
+
